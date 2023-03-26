31694ad370f14475afaa989014521437d6352b96
alison-martinez <alison.a.martinez@gmail.com>
2023-03-26 15:32:23 -0400

    word change

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -115,7 +115,7 @@ export default function Projects() {
       </Head>
       <SimpleLayout
         title="These are a few of my favorite things."
-        intro="I’ve worked on countless projects over the years, but here is a small selection of recent projects and professional publications."
+        intro="I’ve worked on countless projects over the years, but here is a small selection of recent endeavors and professional publications."
       >
         <ul
           role="list"
